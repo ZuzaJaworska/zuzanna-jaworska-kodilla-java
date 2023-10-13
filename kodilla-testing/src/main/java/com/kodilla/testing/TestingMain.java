2f04915d73fa6224cf699befa5b9f564830a6264
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2023-10-13 15:12:44 +0200

    6.2 Zadanie: First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(5, 7);
+        if (resultAdd == 12) {
+            System.out.println("adding - test OK");
+        } else {
+            System.out.println("adding - error");
+        }
+
+        int resultSubstract = calculator.subtract(5, 3);
+        if (resultSubstract == 2) {
+            System.out.println("substring - test OK");
+        } else {
+            System.out.println("substracting - error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+/*
+    int a;
+    int b;
+
+    public Calculator() {
+
+    }
+*/
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
