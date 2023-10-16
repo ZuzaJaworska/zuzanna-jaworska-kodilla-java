540bcfe28e0fd2ada884ff6a8c0ac34210d2eed3
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2023-10-16 17:49:45 +0200

    6.3 Zadanie: JUnit, czyli pierwsze testy

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -2,7 +2,6 @@ package com.kodilla.testing.collection;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 
 import org.junit.jupiter.api.*;
-
 import java.util.*;
 
 class CollectionTestSuite {
