8b55decd959349a2b298d034a0f1ebd2533254c3
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2023-10-22 18:26:47 +0200

    7.1 Lambda - Zadanie: Beautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,24 +1,22 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        String line = "I love \"Disc world\"";
+        System.out.println("Let's beautify this line - " + line);
+        poemBeautifier.beautify(line, text -> "ABC  " + line + "  ABC");
+        poemBeautifier.beautify(line, text -> line.toUpperCase());
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        poemBeautifier.beautify(line, text -> "--- " + line + " ---");
+        poemBeautifier.beautify(line, text -> line.toLowerCase());
+        poemBeautifier.beautify(line, text -> line.repeat(3));
+        poemBeautifier.beautify(line, text -> line.replace('"', '/'));
+        poemBeautifier.beautify(line, text -> line.concat("!!!"));
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemDecorator.java
@@ -0,0 +1,8 @@
+package com.kodilla.stream.beautifier;
+
+import java.lang.*;
+
+public interface PoemDecorator {
+
+    public String decorate(String text);
+}
