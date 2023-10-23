af012e82cfeb9bf52376175ff5ee8df46a1d112a
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2023-10-23 12:31:24 +0200

    7.3 Funkcje Stream - base

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,26 +1,22 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        BookDirectory theBookDirectory = new BookDirectory();
 
-        String line = "I love \"Disc world\"";
-        System.out.println("Let's beautify this line - " + line);
-        poemBeautifier.beautify(line, text -> "ABC  " + line + "  ABC");
-        poemBeautifier.beautify(line, text -> line.toUpperCase());
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));
 
-        poemBeautifier.beautify(line, text -> "--- " + line + " ---");
-        poemBeautifier.beautify(line, text -> line.toLowerCase());
-        poemBeautifier.beautify(line, text -> line.repeat(3));
-        poemBeautifier.beautify(line, text -> line.replace('"', '/'));
-        poemBeautifier.beautify(line, text -> line.concat("!!!"));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultStringOfBooks);
     }
 }
