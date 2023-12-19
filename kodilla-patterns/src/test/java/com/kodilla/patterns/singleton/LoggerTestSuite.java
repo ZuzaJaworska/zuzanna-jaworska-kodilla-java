d2d8c0dc9fa3b7c5083dee61c425b95f0f036d4d
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2023-12-19 19:27:13 +0100

    14.1 Enum Singleton - Zadanie: Logger

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    @Test
+    void testGetLastLog() {
+        //Given
+        //When
+        Logger.getInstance().log("testLogName");
+        String resultName = Logger.getInstance().getLastLog();
+        //Then
+        assertEquals("testLogName", resultName);
+    }
+}
