663debece228973ffd71064b4d3e302e6a4b8249
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2024-01-22 15:14:23 +0100

    15.6 MySQL - Zadanie: aktywni uzytkownicy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -69,4 +69,31 @@ class DbManagerTestSuite {
         statement.close();
         assertEquals(20, counter);
     }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = """
+                SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER
+                FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID
+                GROUP BY P.USER_ID
+                HAVING COUNT(*) >= 2""";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                               rs.getString("LASTNAME") + ", " +
+                               rs.getInt("POSTS_NUMBER"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(1, counter);
+    }
 }
