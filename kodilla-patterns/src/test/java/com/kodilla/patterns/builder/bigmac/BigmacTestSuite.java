8f3619acfa0a4a523b7e8365220f850af90fe321
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2024-01-29 18:32:04 +0100

    14.5 Builder - Zadanie: BigMac builder - poprawiony test

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -2,9 +2,11 @@ package com.kodilla.patterns.builder.bigmac;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BigmacTestSuite {
+
+    private static final String BUN = "BUN_WITH_SESAME";
 
     @Test
     void testBigmacNew() {
@@ -21,9 +23,25 @@ public class BigmacTestSuite {
         System.out.println(bigmac);
 
         //When
+        int howManyPatties = bigmac.getPattiesQty();
         int howManyIngredients = bigmac.getIngredients().size();
 
+
         //Then
+        assertEquals(2, howManyPatties);
         assertEquals(4, howManyIngredients);
+
+        assertEquals("BUN_WITH_SESAME", bigmac.getBun().toString());
+        assertEquals("BBQ", bigmac.getSauce().toString());
+
+        assertTrue(bigmac.getIngredients().contains(Ingredients.BACON));
+        assertTrue(bigmac.getIngredients().contains(Ingredients.CHEESE));
+        assertTrue(bigmac.getIngredients().contains(Ingredients.ONION));
+        assertTrue(bigmac.getIngredients().contains(Ingredients.LETTUCE));
+
+        assertFalse(bigmac.getIngredients().contains(Ingredients.PICKLE));
+        assertFalse(bigmac.getIngredients().contains(Ingredients.CHILLI_PEPPERS));
+        assertFalse(bigmac.getIngredients().contains(Ingredients.MUSHROOMS));
+        assertFalse(bigmac.getIngredients().contains(Ingredients.SHRIMPS));
     }
 }
